b3567c02c42032c694c8b61995d89aab53fd7f29
kovacsv <viktorkovacs@gmail.com>
2017-12-10 13:22:45 +0100

    Separate framework for visual tests.

---
+++ Sources/NodeEngineTest/VisualTestFramework.cpp
@@ -0,0 +1,406 @@
+#include "VisualTestFramework.hpp"
+#include "MouseEventHandler.hpp"
+#include "SimpleTest.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+static std::wstring ReplaceAll (const std::wstring& string, const std::wstring& from, const std::wstring& to)
+{
+	std::wstring result = string;
+	size_t found = string.find (from);
+	while (found != std::wstring::npos) {
+		result.replace (found, from.length (), to);
+		found = result.find (from);
+	}
+	return result;
+}
+
+SVGBuilder::SVGBuilder ()
+{
+	
+}
+
+void SVGBuilder::AddTag (const std::wstring& tag, const std::vector<std::pair<std::wstring, std::wstring>>& attributes)
+{
+	AddTag (tag, L"", attributes);
+}
+
+void SVGBuilder::AddTag (const std::wstring& tag, const std::wstring& content, const std::vector<std::pair<std::wstring, std::wstring>>& attributes)
+{
+	svgContent << L"<" << tag;
+	AddAttributes (attributes);
+	if (content.empty ()) {
+		svgContent << L"/>" << std::endl;
+	} else {
+		std::wstring contentToWrite = content;
+		contentToWrite = ReplaceAll (contentToWrite, L"<", L"&lt;");
+		contentToWrite = ReplaceAll (contentToWrite, L">", L"&gt;");
+		svgContent << L">" << contentToWrite;
+		AddCloseTag (tag);
+	}
+}
+
+void SVGBuilder::AddOpenTag (const std::wstring& tag, const std::vector<std::pair<std::wstring, std::wstring>>& attributes)
+{
+	svgContent << L"<" << tag;
+	AddAttributes (attributes);
+	svgContent << L">" << std::endl;
+}
+
+void SVGBuilder::AddCloseTag (const std::wstring& tag)
+{
+	svgContent << L"</" << tag << L">" << std::endl;
+}
+
+void SVGBuilder::Clear ()
+{
+	svgContent.str (std::wstring ());
+}
+
+void SVGBuilder::WriteToFile (const std::string& fileName) const
+{
+	std::wofstream file;
+	file.open (fileName);
+	file << svgContent.str ();
+	file.close ();
+}
+
+std::wstring SVGBuilder::GetAsString () const
+{
+	return svgContent.str ();
+}
+
+std::wstring SVGBuilder::ToString (double val)
+{
+	return std::to_wstring ((int) ceil (val));
+}
+
+std::wstring SVGBuilder::BegToString (double val)
+{
+	return std::to_wstring ((int) floor (val));
+}
+
+std::wstring SVGBuilder::EndToString (double val)
+{
+	return std::to_wstring ((int) ceil (val));
+}
+
+std::wstring SVGBuilder::PenToStrokeStyle (const Pen& pen)
+{
+	std::wstring style = L"";
+	style += L"stroke-width:" + ToString (pen.GetThickness ()) + L"px;";
+	style += L"stroke:rgb(" + ToString (pen.GetColor ().GetR ()) + L"," + ToString (pen.GetColor ().GetG ()) + L"," + ToString (pen.GetColor ().GetB ()) + L");";
+	return style;
+}
+
+std::wstring SVGBuilder::ColorToFillStyle (const Color& color)
+{
+	std::wstring style = L"";
+	style += L"fill:rgb(" + ToString (color.GetR ()) + L"," + ToString (color.GetG ()) + L"," + ToString (color.GetB ()) + L");";
+	return style;
+}
+
+std::wstring SVGBuilder::FontToFontStyle (const Font& font)
+{
+	std::wstring style = L"";
+	style += L"font-family:" + font.GetFamily () + L";";
+	style += L"font-size:" + SVGBuilder::ToString (font.GetSize ()) + L"px;";
+	return style;
+}
+
+std::wstring SVGBuilder::PointToPath (const Point& point)
+{
+	return ToString (point.GetX ()) + L"," + ToString (point.GetY ());
+}
+
+void SVGBuilder::AddAttributes (const std::vector<std::pair<std::wstring, std::wstring>>& attributes)
+{
+	for (const auto& it : attributes) {
+		svgContent << L" " << it.first << L"=\"" << it.second << L"\"";
+	}
+}
+
+SVGDrawingContext::SVGDrawingContext (double width, double height) :
+	width (width),
+	height (height)
+{
+	
+}
+
+void SVGDrawingContext::WriteToFile (const std::string& fileName) const
+{
+	svgBuilder.WriteToFile (fileName);
+}
+
+std::wstring SVGDrawingContext::GetAsString () const
+{
+	return svgBuilder.GetAsString ();
+}
+
+void SVGDrawingContext::Resize (int, int)
+{
+	
+}
+
+double SVGDrawingContext::GetWidth () const
+{
+	return width;
+}
+
+double SVGDrawingContext::GetHeight () const
+{
+	return height;
+}
+	
+void SVGDrawingContext::BeginDraw ()
+{
+	svgBuilder.Clear ();
+	svgBuilder.AddOpenTag (L"svg", {
+		{ L"version", L"1.1" },
+		{ L"xmlns", L"http://www.w3.org/2000/svg" },
+		{ L"width", SVGBuilder::ToString (GetWidth ()) },
+		{ L"height", SVGBuilder::ToString (GetHeight ()) },
+		{ L"shape-rendering", L"crispEdges" }
+	});
+}
+
+void SVGDrawingContext::EndDraw ()
+{
+	svgBuilder.AddCloseTag (L"svg");
+}
+				 
+void SVGDrawingContext::DrawLine (const Point&, const Point&, const Pen&)
+{
+	DBGBREAK ();
+}
+
+void SVGDrawingContext::DrawBezier (const Point& p1, const Point& p2, const Point& p3, const Point& p4, const Pen& pen)
+{
+	svgBuilder.AddTag (L"path", {
+		{ L"d", L"M" + SVGBuilder::PointToPath (p1) + L" C" + SVGBuilder::PointToPath (p2) + L" " + SVGBuilder::PointToPath (p3) + L" " + SVGBuilder::PointToPath (p4) },
+		{ L"style", L"fill:none;" + SVGBuilder::PenToStrokeStyle (pen) }
+	});
+}
+				 
+void SVGDrawingContext::DrawRect (const Rect& rect, const Pen& pen)
+{
+	svgBuilder.AddTag (L"rect", {
+		{ L"x", SVGBuilder::BegToString (rect.GetX ()) },
+		{ L"y", SVGBuilder::BegToString (rect.GetY ()) },
+		{ L"width", SVGBuilder::EndToString (rect.GetWidth ()) },
+		{ L"height", SVGBuilder::EndToString (rect.GetHeight ()) },
+		{ L"style", L"fill:none;" + SVGBuilder::PenToStrokeStyle (pen) }
+	});
+}
+
+void SVGDrawingContext::FillRect (const Rect& rect, const Color& color)
+{
+	svgBuilder.AddTag (L"rect", {
+		{ L"x", SVGBuilder::BegToString (rect.GetX ()) },
+		{ L"y", SVGBuilder::BegToString (rect.GetY ()) },
+		{ L"width", SVGBuilder::EndToString (rect.GetWidth ()) },
+		{ L"height", SVGBuilder::EndToString (rect.GetHeight ()) },
+		{ L"style", SVGBuilder::ColorToFillStyle (color) }
+	});
+}
+				 
+void SVGDrawingContext::DrawEllipse (const Rect&, const Pen&)
+{
+	DBGBREAK ();
+}
+
+void SVGDrawingContext::FillEllipse (const Rect&, const Color&)
+{
+	DBGBREAK ();
+}
+				 
+void SVGDrawingContext::DrawFormattedText (const Rect& rect, const Font& font, const std::wstring& text, HorizontalAnchor hAnchor, VerticalAnchor vAnchor, const Color& color)
+{
+	std::wstring textAnchor;
+	std::wstring dominantBaseline;
+	double x = 0.0;
+	double y = 0.0;
+
+	switch (hAnchor) {
+	case HorizontalAnchor::Left:
+		textAnchor = L"start";
+		x = rect.GetLeft ();
+		break;
+	case HorizontalAnchor::Center:
+		textAnchor = L"middle";
+		x = rect.GetCenter ().GetX ();
+		break;
+	case HorizontalAnchor::Right:
+		textAnchor = L"end";
+		x = rect.GetRight ();
+		break;
+	}
+
+	switch (vAnchor) {
+	case VerticalAnchor::Top:
+		dominantBaseline = L"text-before-edge";
+		y = rect.GetTop ();
+		break;
+	case VerticalAnchor::Center:
+		dominantBaseline = L"central";
+		y = rect.GetCenter ().GetY ();
+		break;
+	case VerticalAnchor::Bottom:
+		dominantBaseline = L"text-after-edge";
+		y = rect.GetBottom ();
+		break;
+	}
+
+	svgBuilder.AddTag (L"text", text, {
+		{ L"x", SVGBuilder::BegToString (x) },
+		{ L"y", SVGBuilder::BegToString (y) },
+		{ L"dominant-baseline", dominantBaseline },
+		{ L"text-anchor", textAnchor },
+		{ L"style", SVGBuilder::ColorToFillStyle (color) + SVGBuilder::FontToFontStyle (font) }
+	});
+}
+
+Size SVGDrawingContext::MeasureText (const Font& font, const std::wstring& text)
+{
+	return Size (text.length () * font.GetSize (), font.GetSize () * 1.5);
+}
+
+void SVGDrawingContext::OnNodeDrawingBegin (const NodeId&, const NE::Checksum&, const Rect&)
+{
+		
+}
+
+void SVGDrawingContext::OnNodeDrawingEnd (const NodeId&)
+{
+		
+}
+
+TestEventHandlers::TestEventHandlers ()
+{
+	
+}
+
+void TestEventHandlers::OnRecalculate ()
+{
+	
+}
+
+void TestEventHandlers::OnRedraw ()
+{
+	
+}
+
+CommandPtr TestEventHandlers::OnContextMenu (NodeUIManager&, NodeUIEnvironment&, const Point&, const CommandStructure&)
+{
+	return nullptr;
+}
+
+CommandPtr TestEventHandlers::OnContextMenu (NodeUIManager&, NodeUIEnvironment&, const Point&, const UINodePtr&, const CommandStructure&)
+{
+	return nullptr;
+}
+
+CommandPtr TestEventHandlers::OnContextMenu (NodeUIManager&, NodeUIEnvironment&, const Point&, const NE::OutputSlotPtr&, const CommandStructure&)
+{
+	return nullptr;
+}
+
+CommandPtr TestEventHandlers::OnContextMenu (NodeUIManager&, NodeUIEnvironment&, const Point&, const NE::InputSlotPtr&, const CommandStructure&)
+{
+	return nullptr;
+}
+
+TestNodeEditorInterface::TestNodeEditorInterface () :
+	drawingContext (800, 600),
+	eventHandlers (),
+	evaluationEnv (nullptr)
+{
+	
+}
+
+const SVGDrawingContext& TestNodeEditorInterface::GetSVGDrawingContext () const
+{
+	return drawingContext;
+}
+
+NodeDrawingContext& TestNodeEditorInterface::GetDrawingContext ()
+{
+	return drawingContext;
+}
+
+EventHandlers& TestNodeEditorInterface::GetEventHandlers ()
+{
+	return eventHandlers;
+}
+
+EvaluationEnv& TestNodeEditorInterface::GetEvaluationEnv ()
+{
+	return evaluationEnv;
+}
+
+NodeEditorTestEnv::NodeEditorTestEnv () :
+	nodeEditorInterface (),
+	nodeEditor (nodeEditorInterface)
+{
+}
+
+bool NodeEditorTestEnv::CheckReference (const std::string& referenceFileName)
+{
+	const SVGDrawingContext& context = nodeEditorInterface.GetSVGDrawingContext ();
+
+	std::string testFilesPath = SimpleTest::GetAppFolderLocation () + "VisualTestFiles" + PATH_SEPARATOR;
+	std::string referenceFilePath = testFilesPath + referenceFileName;
+	std::wifstream referenceFile;
+	referenceFile.open (referenceFilePath);
+	if (!referenceFile.is_open ()) {
+		context.WriteToFile (testFilesPath + "Current_" + referenceFileName);
+		return false;
+	}
+
+	std::wstringstream referenceFileBuffer;
+	referenceFileBuffer << referenceFile.rdbuf ();
+	std::wstring referenceContent = referenceFileBuffer.str ();
+	referenceFile.close ();
+
+	std::wstring currentContent = context.GetAsString ();
+	referenceContent = ReplaceAll (referenceContent, L"\r\n", L"\n");
+	currentContent = ReplaceAll (currentContent, L"\r\n", L"\n");
+	if (referenceContent != currentContent) {
+		std::wcout << std::endl << L"=== CURRENT ===" << std::endl;
+		std::wcout << currentContent << std::endl;
+		std::wcout << L"=== REFERENCE ===" << std::endl;
+		std::wcout << referenceContent << std::endl;
+		context.WriteToFile (testFilesPath + "Current_" + referenceFileName);
+		return false;
+	}
+	return true;
+}
+
+void NodeEditorTestEnv::Click (const Point& point)
+{
+	nodeEditor.OnMouseDown (EmptyKeySet, MouseButton::Left, (int) point.GetX (), (int) point.GetY ());
+	nodeEditor.OnMouseUp (EmptyKeySet, MouseButton::Left, (int) point.GetX (), (int) point.GetY ());
+}
+
+void NodeEditorTestEnv::CtrlClick (const Point& point)
+{
+	nodeEditor.OnMouseDown (KeySet ({ KeyCode::Control }), MouseButton::Left, (int) point.GetX (), (int) point.GetY ());
+	nodeEditor.OnMouseUp (KeySet ({ KeyCode::Control }), MouseButton::Left, (int) point.GetX (), (int) point.GetY ());
+}
+
+void NodeEditorTestEnv::Wheel (MouseWheelRotation rotation, const Point& point)
+{
+	nodeEditor.OnMouseWheel (EmptyKeySet, rotation, (int) point.GetX (), (int) point.GetY ());
+}
+
+void NodeEditorTestEnv::DragDrop (const Point& from, const Point& to, const std::function<void ()>& beforeMouseUp)
+{
+	nodeEditor.OnMouseDown (EmptyKeySet, MouseButton::Left, (int) from.GetX (), (int) from.GetY ());
+	nodeEditor.OnMouseMove (EmptyKeySet, (int) to.GetX (), (int) to.GetY ());
+	if (beforeMouseUp != nullptr) {
+		beforeMouseUp ();
+	}
+	nodeEditor.OnMouseUp (EmptyKeySet, MouseButton::Left, (int) to.GetX (), (int) to.GetY ());
+}
---
+++ Sources/NodeEngineTest/VisualTestFramework.hpp
@@ -0,0 +1,121 @@
+#ifndef VISUALTESTFRAMEWORK_HPP
+#define VISUALTESTFRAMEWORK_HPP
+
+#include "NodeDrawingContext.hpp"
+#include "EvaluationEnv.hpp"
+#include "EventHandlers.hpp"
+#include "NodeEditor.hpp"
+
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace NE;
+using namespace NUIE;
+
+class SVGBuilder
+{
+public:
+	SVGBuilder ();
+
+	void AddTag (const std::wstring& tag, const std::vector<std::pair<std::wstring, std::wstring>>& attributes);
+	void AddTag (const std::wstring& tag, const std::wstring& content, const std::vector<std::pair<std::wstring, std::wstring>>& attributes);
+
+	void AddOpenTag (const std::wstring& tag, const std::vector<std::pair<std::wstring, std::wstring>>& attributes);
+	void AddCloseTag (const std::wstring& tag);
+
+	void Clear ();
+	void WriteToFile (const std::string& fileName) const;
+
+	std::wstring GetAsString () const;
+
+	static std::wstring ToString (double val);
+	static std::wstring BegToString (double val);
+	static std::wstring EndToString (double val);
+	static std::wstring PenToStrokeStyle (const Pen& pen);
+	static std::wstring ColorToFillStyle (const Color& color);
+	static std::wstring FontToFontStyle (const Font& font);
+	static std::wstring PointToPath (const Point& point);
+
+private:
+	void AddAttributes (const std::vector<std::pair<std::wstring, std::wstring>>& attributes);
+
+	std::wostringstream svgContent;
+};
+
+class SVGDrawingContext : public NodeDrawingContext
+{
+public:
+	SVGDrawingContext (double width, double height);
+
+	void				WriteToFile (const std::string& fileName) const;
+	std::wstring		GetAsString () const;
+
+	virtual void		Resize (int, int) override;
+	virtual double		GetWidth () const override;
+	virtual double		GetHeight () const override;
+	virtual void		BeginDraw () override;
+	virtual void		EndDraw () override;
+	virtual void		DrawLine (const Point&, const Point&, const Pen&) override;
+	virtual void		DrawBezier (const Point& p1, const Point& p2, const Point& p3, const Point& p4, const Pen& pen) override;
+	virtual void		DrawRect (const Rect& rect, const Pen& pen) override;
+	virtual void		FillRect (const Rect& rect, const Color& color) override;
+	virtual void		DrawEllipse (const Rect&, const Pen&) override;
+	virtual void		FillEllipse (const Rect&, const Color&) override;
+	virtual void		DrawFormattedText (const Rect& rect, const Font& font, const std::wstring& text, HorizontalAnchor hAnchor, VerticalAnchor vAnchor, const Color& color) override;
+	virtual Size		MeasureText (const Font& font, const std::wstring& text) override;
+	virtual void		OnNodeDrawingBegin (const NodeId&, const NE::Checksum&, const Rect&) override;
+	virtual void		OnNodeDrawingEnd (const NodeId&) override;
+
+private:
+	SVGBuilder	svgBuilder;
+	double		width;
+	double		height;
+};
+
+
+class TestEventHandlers : public EventHandlers
+{
+public:
+	TestEventHandlers ();
+
+	virtual void		OnRecalculate () override;
+	virtual void		OnRedraw () override;
+	virtual CommandPtr	OnContextMenu (NodeUIManager&, NodeUIEnvironment&, const Point&, const CommandStructure&) override;
+	virtual CommandPtr	OnContextMenu (NodeUIManager&, NodeUIEnvironment&, const Point&, const UINodePtr&, const CommandStructure&) override;
+	virtual CommandPtr	OnContextMenu (NodeUIManager&, NodeUIEnvironment&, const Point&, const NE::OutputSlotPtr&, const CommandStructure&) override;
+	virtual CommandPtr	OnContextMenu (NodeUIManager&, NodeUIEnvironment&, const Point&, const NE::InputSlotPtr&, const CommandStructure&) override;
+};
+
+
+class TestNodeEditorInterface : public NodeEditorInterface
+{
+public:
+	TestNodeEditorInterface ();
+	const SVGDrawingContext&		GetSVGDrawingContext () const;
+	virtual NodeDrawingContext&		GetDrawingContext () override;
+	virtual EventHandlers&			GetEventHandlers () override;
+	virtual EvaluationEnv&			GetEvaluationEnv () override;
+
+private:
+	SVGDrawingContext	drawingContext;
+	TestEventHandlers	eventHandlers;
+	EvaluationEnv		evaluationEnv;
+};
+
+class NodeEditorTestEnv
+{
+public:
+	NodeEditorTestEnv ();
+
+	bool	CheckReference (const std::string& referenceFileName);
+	void	Click (const Point& point);
+	void	CtrlClick (const Point& point);
+	void	Wheel (MouseWheelRotation rotation, const Point& point);
+	void	DragDrop (const Point& from, const Point& to, const std::function<void ()>& beforeMouseUp = nullptr);
+
+	TestNodeEditorInterface		nodeEditorInterface;
+	NodeEditor					nodeEditor;
+};
+
+#endif
