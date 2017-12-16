ff315a4d0193862fcff6612076fda1b6fe3465d2
kovacsv <kowik1984@gmail.com>
2017-12-16 17:50:22 +0100

    Minor modification.

--- Sources/WindowsAppSupport/Headers/BitmapContextGdiplus.hpp
+++ Sources/WindowsAppSupport/Headers/BitmapContextGdiplus.hpp
@@ -12,28 +12,28 @@ public:
 	BitmapContextGdiplus (int width, int height);
 	virtual ~BitmapContextGdiplus ();
 
-	virtual void				Resize (int newWidth, int newHeight) override;
+	virtual void		Resize (int newWidth, int newHeight) override;
 
-	virtual double				GetWidth () const override;
-	virtual double				GetHeight () const override;
+	virtual double		GetWidth () const override;
+	virtual double		GetHeight () const override;
 
-	virtual void				BeginDraw () override;
-	virtual void				EndDraw () override;
+	virtual void		BeginDraw () override;
+	virtual void		EndDraw () override;
 
-	virtual void				DrawLine (const NUIE::Point& beg, const NUIE::Point& end, const NUIE::Pen& pen) override;
-	virtual void				DrawBezier (const NUIE::Point& p1, const NUIE::Point& p2, const NUIE::Point& p3, const NUIE::Point& p4, const NUIE::Pen& pen) override;
+	virtual void		DrawLine (const NUIE::Point& beg, const NUIE::Point& end, const NUIE::Pen& pen) override;
+	virtual void		DrawBezier (const NUIE::Point& p1, const NUIE::Point& p2, const NUIE::Point& p3, const NUIE::Point& p4, const NUIE::Pen& pen) override;
 
-	virtual void				DrawRect (const NUIE::Rect& rect, const NUIE::Pen& pen) override;
-	virtual void				FillRect (const NUIE::Rect& rect, const NUIE::Color& color) override;
+	virtual void		DrawRect (const NUIE::Rect& rect, const NUIE::Pen& pen) override;
+	virtual void		FillRect (const NUIE::Rect& rect, const NUIE::Color& color) override;
 
-	virtual void				DrawEllipse (const NUIE::Rect& rect, const NUIE::Pen& pen) override;
-	virtual void				FillEllipse (const NUIE::Rect& rect, const NUIE::Color& color) override;
+	virtual void		DrawEllipse (const NUIE::Rect& rect, const NUIE::Pen& pen) override;
+	virtual void		FillEllipse (const NUIE::Rect& rect, const NUIE::Color& color) override;
 
-	virtual void				DrawFormattedText (const NUIE::Rect& rect, const NUIE::Font& font, const std::wstring& text, NUIE::HorizontalAnchor hAnchor, NUIE::VerticalAnchor vAnchor, const NUIE::Color& backgroundColor, const NUIE::Color& textColor) override;
-	virtual NUIE::Size			MeasureText (const NUIE::Font& font, const std::wstring& text) override;
+	virtual void		DrawFormattedText (const NUIE::Rect& rect, const NUIE::Font& font, const std::wstring& text, NUIE::HorizontalAnchor hAnchor, NUIE::VerticalAnchor vAnchor, const NUIE::Color& backgroundColor, const NUIE::Color& textColor) override;
+	virtual NUIE::Size	MeasureText (const NUIE::Font& font, const std::wstring& text) override;
 
-	void						Setup (HWND hwnd);
-	void						DrawToHDC (HDC hdc);
+	void				Setup (HWND hwnd);
+	void				DrawToHDC (HDC hdc);
 
 private:
 	Gdiplus::Point		CreatePoint (const NUIE::Point& point) const;
