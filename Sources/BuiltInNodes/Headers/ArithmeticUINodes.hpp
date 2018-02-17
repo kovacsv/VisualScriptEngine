8b6ca61d6720a85d8a6ab572d0dff937af488d45
kovacsv <kowik1984@gmail.com>
2018-02-17 22:26:55 +0100

    Introducing BinaryOperationNode.

--- Sources/BuiltInNodes/Headers/ArithmeticUINodes.hpp
+++ Sources/BuiltInNodes/Headers/ArithmeticUINodes.hpp
@@ -10,7 +10,27 @@
 namespace NUIE
 {
 
-class AdditionNode : public HeaderWithSlotsUINode
+class BinaryOperationNode : public HeaderWithSlotsUINode
+{
+	SERIALIZABLE;
+
+public:
+	BinaryOperationNode ();
+	BinaryOperationNode (const std::wstring& name, const Point& position);
+	virtual ~BinaryOperationNode ();
+
+	virtual void				RegisterSlots () override;
+	virtual NE::ValuePtr		Calculate (NE::EvaluationEnv& env) const override;
+	virtual void				RegisterParameters (NodeParameterList& parameterList) const;
+
+	virtual NE::Stream::Status	Read (NE::InputStream& inputStream) override;
+	virtual NE::Stream::Status	Write (NE::OutputStream& outputStream) const override;
+
+private:
+	virtual double				DoOperation (double a, double b) const = 0;
+};
+
+class AdditionNode : public BinaryOperationNode
 {
 	DYNAMIC_SERIALIZABLE (AdditionNode);
 
@@ -19,12 +39,8 @@ public:
 	AdditionNode (const std::wstring& name, const Point& position);
 	virtual ~AdditionNode ();
 
-	virtual void				RegisterSlots () override;
-	virtual NE::ValuePtr		Calculate (NE::EvaluationEnv& env) const override;
-	virtual void				RegisterParameters (NodeParameterList& parameterList) const;
-
-	virtual NE::Stream::Status	Read (NE::InputStream& inputStream) override;
-	virtual NE::Stream::Status	Write (NE::OutputStream& outputStream) const override;
+private:
+	virtual double DoOperation (double a, double b) const override;
 };
 
 }
--- Sources/BuiltInNodes/Sources/ArithmeticUINodes.cpp
+++ Sources/BuiltInNodes/Sources/ArithmeticUINodes.cpp
@@ -4,33 +4,34 @@
 namespace NUIE
 {
 
-NE::DynamicSerializationInfo	AdditionNode::serializationInfo (NE::ObjectId ("{B54F8625-C6B0-4570-9166-220207C84312}"), NE::ObjectVersion (1), AdditionNode::CreateSerializableInstance);
+NE::SerializationInfo			BinaryOperationNode::serializationInfo (NE::ObjectId ("{B54F8625-C6B0-4570-9166-220207C84312}"), NE::ObjectVersion (1));
+NE::DynamicSerializationInfo	AdditionNode::serializationInfo (NE::ObjectId ("{1A72C230-3D90-42AD-835A-43306E641EA2}"), NE::ObjectVersion (1), AdditionNode::CreateSerializableInstance);
 
-AdditionNode::AdditionNode () :
+BinaryOperationNode::BinaryOperationNode () :
 	HeaderWithSlotsUINode ()
 {
 
 }
 
-AdditionNode::AdditionNode (const std::wstring& name, const Point& position) :
+BinaryOperationNode::BinaryOperationNode (const std::wstring& name, const Point& position) :
 	HeaderWithSlotsUINode (name, position)
 {
 
 }
 
-AdditionNode::~AdditionNode ()
+BinaryOperationNode::~BinaryOperationNode ()
 {
 
 }
 
-void AdditionNode::RegisterSlots ()
+void BinaryOperationNode::RegisterSlots ()
 {
 	RegisterUIInputSlot (UIInputSlotPtr (new UIInputSlot (NE::SlotId ("a"), L"A", NE::ValuePtr (new NE::DoubleValue (0.0)), NE::OutputSlotConnectionMode::Single)));
 	RegisterUIInputSlot (UIInputSlotPtr (new UIInputSlot (NE::SlotId ("b"), L"B", NE::ValuePtr (new NE::DoubleValue (0.0)), NE::OutputSlotConnectionMode::Single)));
 	RegisterUIOutputSlot (UIOutputSlotPtr (new UIOutputSlot (NE::SlotId ("result"), L"Result")));
 }
 
-NE::ValuePtr AdditionNode::Calculate (NE::EvaluationEnv& env) const
+NE::ValuePtr BinaryOperationNode::Calculate (NE::EvaluationEnv& env) const
 {
 	NE::ValuePtr aValue = EvaluateSingleInputSlot (NE::SlotId ("a"), env);
 	NE::ValuePtr bValue = EvaluateSingleInputSlot (NE::SlotId ("b"), env);
@@ -43,25 +44,47 @@ NE::ValuePtr AdditionNode::Calculate (NE::EvaluationEnv& env) const
 	return NE::ValuePtr (new NE::DoubleValue (aDouble + bDouble));
 }
 
-void AdditionNode::RegisterParameters (NodeParameterList& parameterList) const
+void BinaryOperationNode::RegisterParameters (NodeParameterList& parameterList) const
 {
 	HeaderWithSlotsUINode::RegisterParameters (parameterList);
-	RegisterSlotDefaultValueParameter<AdditionNode, NE::DoubleValue> (parameterList, "AdditionNodeAParameter", L"A", ParameterType::Double, NE::SlotId ("a"));
-	RegisterSlotDefaultValueParameter<AdditionNode, NE::DoubleValue> (parameterList, "AdditionNodeBParameter", L"B", ParameterType::Double, NE::SlotId ("b"));
+	RegisterSlotDefaultValueParameter<BinaryOperationNode, NE::DoubleValue> (parameterList, "BinaryOperationNodeAParameter", L"A", ParameterType::Double, NE::SlotId ("a"));
+	RegisterSlotDefaultValueParameter<BinaryOperationNode, NE::DoubleValue> (parameterList, "BinaryOperationNodeBParameter", L"B", ParameterType::Double, NE::SlotId ("b"));
 }
 
-NE::Stream::Status AdditionNode::Read (NE::InputStream& inputStream)
+NE::Stream::Status BinaryOperationNode::Read (NE::InputStream& inputStream)
 {
 	NE::ObjectHeader header (inputStream);
 	HeaderWithSlotsUINode::Read (inputStream);
 	return inputStream.GetStatus ();
 }
 
-NE::Stream::Status AdditionNode::Write (NE::OutputStream& outputStream) const
+NE::Stream::Status BinaryOperationNode::Write (NE::OutputStream& outputStream) const
 {
 	NE::ObjectHeader header (outputStream, serializationInfo);
 	HeaderWithSlotsUINode::Write (outputStream);
 	return outputStream.GetStatus ();
+}
+
+AdditionNode::AdditionNode () :
+	BinaryOperationNode ()
+{
+
+}
+
+AdditionNode::AdditionNode (const std::wstring& name, const Point& position) :
+	BinaryOperationNode (name, position)
+{
+
+}
+
+AdditionNode::~AdditionNode ()
+{
+
+}
+
+double AdditionNode::DoOperation (double a, double b) const
+{
+	return a + b;
 }
 
 }
