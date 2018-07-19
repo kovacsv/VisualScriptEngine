6e82d2bb805ea1cfa1dbab57f568aa6e02bf9361
kovacsv <kowik1984@gmail.com>
2018-07-19 21:05:54 +0200

    Add multi input slot test.

---
+++ Sources/NodeEngineTest/MultiInputSlotTest.cpp
@@ -0,0 +1,213 @@
+#include "SimpleTest.hpp"
+#include "TestNodes.hpp"
+#include "NE_NodeManager.hpp"
+#include "NE_SingleValues.hpp"
+
+using namespace NE;
+
+namespace MultiInputSlotTest
+{
+
+class IntegerNode : public SerializableTestNode
+{
+public:
+	IntegerNode (int val) :
+		SerializableTestNode (),
+		val (val)
+	{
+
+	}
+
+	virtual void RegisterSlots () override
+	{
+		RegisterOutputSlot (OutputSlotPtr (new OutputSlot (SlotId ("out"))));
+	}
+
+	virtual ValuePtr Calculate (NE::EvaluationEnv&) const override
+	{
+		return ValuePtr (new IntValue (val));
+	}
+
+private:
+	int val;
+};
+
+class IntegerListNode : public SerializableTestNode
+{
+public:
+	IntegerListNode (const std::vector<int>& arr) :
+		SerializableTestNode (),
+		arr (arr)
+	{
+
+	}
+
+	virtual void RegisterSlots () override
+	{
+		RegisterOutputSlot (OutputSlotPtr (new OutputSlot (SlotId ("out"))));
+	}
+
+	virtual ValuePtr Calculate (NE::EvaluationEnv&) const override
+	{
+		ListValuePtr result (new ListValue ());
+		for (int val : arr) {
+			result->Push (ValuePtr (new IntValue (val)));
+		}
+		return result;
+	}
+
+private:
+	std::vector<int> arr;
+};
+
+class FlattenerNode : public SerializableTestNode
+{
+public:
+	FlattenerNode () :
+		SerializableTestNode ()
+	{
+
+	}
+
+	virtual void RegisterSlots () override
+	{
+		RegisterInputSlot (InputSlotPtr (new InputSlot (SlotId ("in"), nullptr, OutputSlotConnectionMode::Multiple)));
+		RegisterOutputSlot (OutputSlotPtr (new OutputSlot (SlotId ("out"))));
+	}
+
+	virtual ValuePtr Calculate (NE::EvaluationEnv& env) const override
+	{
+		ListValuePtr val = EvaluateInputSlot (SlotId ("in"), env);
+		ListValuePtr result (new ListValue ());
+
+		bool isValid = true;
+		val->Enumerate ([&] (const ValuePtr& itemVal) {
+			if (!IsComplexType<IntValue> (itemVal)) {
+				isValid = false;
+			}
+		});
+		if (!isValid) {
+			return nullptr;
+		}
+
+		val->Enumerate ([&] (const ValuePtr& itemVal) {
+			IListValuePtr listVal = CreateListValue (itemVal);
+			listVal->Enumerate ([&] (const ValuePtr& listItemVal) {
+				result->Push (listItemVal->Clone ());
+			});
+		});
+		return result;
+	}
+};
+
+TEST (FlattenerNodeTest_SingleValue)
+{
+	NodeManager manager;
+
+	NodePtr integerNode (new IntegerNode (1));
+	NodePtr flattenerNode (new FlattenerNode ());
+
+	manager.AddNode (integerNode);
+	manager.AddNode (flattenerNode);
+	manager.ConnectOutputSlotToInputSlot (integerNode->GetOutputSlot (SlotId ("out")), flattenerNode->GetInputSlot (SlotId ("in")));
+
+	ValuePtr val = flattenerNode->Evaluate (EmptyEvaluationEnv);
+	ListValuePtr listVal = Value::Cast<ListValue> (val);
+	ASSERT (listVal->GetSize () == 1);
+	ASSERT (NE::IntValue::Get (listVal->GetValue (0)) == 1);
+}
+
+TEST (FlattenerNodeTest_MultiValue)
+{
+	NodeManager manager;
+
+	NodePtr integerNode1 (new IntegerNode (1));
+	NodePtr integerNode2 (new IntegerNode (2));
+	NodePtr flattenerNode (new FlattenerNode ());
+
+	manager.AddNode (integerNode1);
+	manager.AddNode (integerNode2);
+	manager.AddNode (flattenerNode);
+	manager.ConnectOutputSlotToInputSlot (integerNode1->GetOutputSlot (SlotId ("out")), flattenerNode->GetInputSlot (SlotId ("in")));
+	manager.ConnectOutputSlotToInputSlot (integerNode2->GetOutputSlot (SlotId ("out")), flattenerNode->GetInputSlot (SlotId ("in")));
+
+	ValuePtr val = flattenerNode->Evaluate (EmptyEvaluationEnv);
+	ListValuePtr listVal = Value::Cast<ListValue> (val);
+	ASSERT (listVal->GetSize () == 2);
+	ASSERT (NE::IntValue::Get (listVal->GetValue (0)) == 1);
+	ASSERT (NE::IntValue::Get (listVal->GetValue (1)) == 2);
+}
+
+TEST (FlattenerNodeTest_SingleListValue)
+{
+	NodeManager manager;
+
+	NodePtr integerListNode (new IntegerListNode ({1, 2}));
+	NodePtr flattenerNode (new FlattenerNode ());
+
+	manager.AddNode (integerListNode);
+	manager.AddNode (flattenerNode);
+	manager.ConnectOutputSlotToInputSlot (integerListNode->GetOutputSlot (SlotId ("out")), flattenerNode->GetInputSlot (SlotId ("in")));
+
+	ValuePtr val = flattenerNode->Evaluate (EmptyEvaluationEnv);
+	ListValuePtr listVal = Value::Cast<ListValue> (val);
+	ASSERT (listVal->GetSize () == 2);
+	ASSERT (NE::IntValue::Get (listVal->GetValue (0)) == 1);
+	ASSERT (NE::IntValue::Get (listVal->GetValue (1)) == 2);
+}
+
+TEST (FlattenerNodeTest_MultiListValue)
+{
+	NodeManager manager;
+
+	NodePtr integerListNode1 (new IntegerListNode ({1, 2}));
+	NodePtr integerListNode2 (new IntegerListNode ({3, 4}));
+	NodePtr flattenerNode (new FlattenerNode ());
+
+	manager.AddNode (integerListNode1);
+	manager.AddNode (integerListNode2);
+	manager.AddNode (flattenerNode);
+	manager.ConnectOutputSlotToInputSlot (integerListNode1->GetOutputSlot (SlotId ("out")), flattenerNode->GetInputSlot (SlotId ("in")));
+	manager.ConnectOutputSlotToInputSlot (integerListNode2->GetOutputSlot (SlotId ("out")), flattenerNode->GetInputSlot (SlotId ("in")));
+
+	ValuePtr val = flattenerNode->Evaluate (EmptyEvaluationEnv);
+	ListValuePtr listVal = Value::Cast<ListValue> (val);
+	ASSERT (listVal->GetSize () == 4);
+	ASSERT (NE::IntValue::Get (listVal->GetValue (0)) == 1);
+	ASSERT (NE::IntValue::Get (listVal->GetValue (1)) == 2);
+	ASSERT (NE::IntValue::Get (listVal->GetValue (2)) == 3);
+	ASSERT (NE::IntValue::Get (listVal->GetValue (3)) == 4);
+}
+
+TEST (FlattenerNodeTest_MixedSingleAndListValue)
+{
+	NodeManager manager;
+
+	NodePtr integerNode1 (new IntegerNode (1));
+	NodePtr integerNode2 (new IntegerNode (2));
+	NodePtr integerListNode1 (new IntegerListNode ({3, 4}));
+	NodePtr integerListNode2 (new IntegerListNode ({5, 6}));
+	NodePtr flattenerNode (new FlattenerNode ());
+
+	manager.AddNode (integerNode1);
+	manager.AddNode (integerNode2);
+	manager.AddNode (integerListNode1);
+	manager.AddNode (integerListNode2);
+	manager.AddNode (flattenerNode);
+	manager.ConnectOutputSlotToInputSlot (integerListNode1->GetOutputSlot (SlotId ("out")), flattenerNode->GetInputSlot (SlotId ("in")));
+	manager.ConnectOutputSlotToInputSlot (integerNode1->GetOutputSlot (SlotId ("out")), flattenerNode->GetInputSlot (SlotId ("in")));
+	manager.ConnectOutputSlotToInputSlot (integerListNode2->GetOutputSlot (SlotId ("out")), flattenerNode->GetInputSlot (SlotId ("in")));
+	manager.ConnectOutputSlotToInputSlot (integerNode2->GetOutputSlot (SlotId ("out")), flattenerNode->GetInputSlot (SlotId ("in")));
+
+	ValuePtr val = flattenerNode->Evaluate (EmptyEvaluationEnv);
+	ListValuePtr listVal = Value::Cast<ListValue> (val);
+	ASSERT (listVal->GetSize () == 6);
+	ASSERT (NE::IntValue::Get (listVal->GetValue (0)) == 3);
+	ASSERT (NE::IntValue::Get (listVal->GetValue (1)) == 4);
+	ASSERT (NE::IntValue::Get (listVal->GetValue (2)) == 1);
+	ASSERT (NE::IntValue::Get (listVal->GetValue (3)) == 5);
+	ASSERT (NE::IntValue::Get (listVal->GetValue (4)) == 6);
+	ASSERT (NE::IntValue::Get (listVal->GetValue (5)) == 2);
+}
+
+}
