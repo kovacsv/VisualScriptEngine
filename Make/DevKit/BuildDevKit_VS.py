9fd4ae9c5ece26be99ebe44dbab8ca2a9629d29d
kovacsv <kowik1984@gmail.com>
2018-10-17 08:25:12 +0200

    Update DevKit builder.

--- Make/DevKit/BuildDevKit_VS.py
+++ Make/DevKit/BuildDevKit_VS.py
@@ -10,69 +10,54 @@ def MakeDir (path):
 	if not os.path.exists (path):
 		os.mkdir (path)	
 
-def GetDevKitDir (rootDir, mode):
-	return os.path.join (rootDir, 'Make', 'DevKit', 'VS_' + mode)
-		
-class DevKitModule:
-	def __init__ (self, rootDir, moduleName):
-		self.rootDir = rootDir
-		self.moduleName = moduleName
-
-	def IsAvailable (self, mode):
-		if not os.path.exists (self.GetHeadersDir ()):
-			return False
-		if not os.path.exists (self.GetLibFilePath (mode)):
-			return False
-		return True
-		
-	def Copy (self, mode):
-		devKitDir = GetDevKitDir (self.rootDir, mode)
-		devKitHeadersDir = os.path.join (devKitDir, 'include')
-		devKitLibsDir = os.path.join (devKitDir, 'lib')
-		MakeDir (devKitHeadersDir)	
-		MakeDir (devKitLibsDir)
-		for headerFile in os.listdir (self.GetHeadersDir ()):
-			shutil.copy (os.path.join (self.GetHeadersDir (), headerFile), devKitHeadersDir)
-		shutil.copy (self.GetLibFilePath (mode), devKitLibsDir)
-	
-	def GetHeadersDir (self):
-		return os.path.join (self.rootDir, 'Sources', self.moduleName, 'Headers')
-	
-	def GetLibFilePath (self, mode):
-		return os.path.join (self.rootDir, 'Make', 'VS', 'x64', mode, self.moduleName + '.lib')
+def RemoveAndMakeDir (path):
+	RemoveDir (path)
+	MakeDir (path)
 		
 def Main (argv):
 	currentDir = os.path.dirname (os.path.abspath (__file__))
 	os.chdir (currentDir)
 
-	rootDir = os.path.abspath (os.path.join (currentDir, '..', '..'))
-
 	modules = [
-		DevKitModule (rootDir, 'NodeEngine'),
-		DevKitModule (rootDir, 'NodeUIEngine'),
-		DevKitModule (rootDir, 'BuiltInNodes'),
-		DevKitModule (rootDir, 'WindowsAppSupport'),
-		DevKitModule (rootDir, 'wxWidgetsAppSupport')
+		'NodeEngine',
+		'NodeUIEngine',
+		'BuiltInNodes',
+		'WindowsAppSupport',
+		'wxWidgetsAppSupport'
 	]
 	
+	rootDir = os.path.abspath (os.path.join (currentDir, '..', '..'))
+	devKitRootDir = os.path.join (rootDir, 'Make', 'DevKit')
+	devKitIncludeDir = os.path.join (devKitRootDir, 'include')
+	
+	RemoveAndMakeDir (devKitIncludeDir)
+	print 'Copy header files'
+	for moduleName in modules:
+		print '  Copy header files for ' + moduleName
+		moduleHeadersDir = os.path.join (rootDir, 'Sources', moduleName, 'Headers')
+		for headerFile in os.listdir (moduleHeadersDir):
+			shutil.copy (os.path.join (moduleHeadersDir, headerFile), devKitIncludeDir)
+	print '  SUCCESSFUL'
+	
 	for mode in ['Debug', 'Release']:
-		devKitDir = GetDevKitDir (rootDir, mode)
-		RemoveDir (devKitDir)
-		MakeDir (devKitDir)
-		print 'Building ' + mode + ' DevKit...'
-		isAvailable = True
-		for module in modules:
-			if not module.IsAvailable (mode):
-				isAvailable = False
-				print '  ' + module.moduleName + ': FAILED'
-			else:
-				module.Copy (mode)
-				print '  ' + module.moduleName + ': SUCCESSFUL'
-		if not isAvailable:
-			print '  FAILED: One or more modules are missing.'
-		else:
-			print '  SUCCESSFUL'
 		print ''
+		print 'Copy ' + mode + ' library files'
+		devKitLibsDir = os.path.join (devKitRootDir, 'vs_' + mode.lower ())
+		RemoveAndMakeDir (devKitLibsDir)
+		success = True
+		for moduleName in modules:
+			print '  Copy library files for ' + moduleName
+			moduleLibFile = os.path.join (rootDir, 'Make', 'VS', 'x64', mode, moduleName + '.lib')
+			if not os.path.exists (moduleLibFile):
+				print '  FAILED: ' + moduleLibFile + ' does not exist'
+				success = False
+			else:
+				shutil.copy (moduleLibFile, devKitLibsDir)
+		if success:
+			print '  SUCCESSFUL'
+		else:
+			print '  FAILED: One ore more module files are missing'
+
 	return 0
 	
 sys.exit (Main (sys.argv))
