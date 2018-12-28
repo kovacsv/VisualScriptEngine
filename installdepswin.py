f3d824943ed7b39f861b564f99633998abc5dd70
kovacsv <viktorkovacs@gmail.com>
2018-12-28 11:08:29 +0100

    Minor refactor in installdepswin.py.

--- installdepswin.py
+++ installdepswin.py
@@ -19,21 +19,8 @@ def UnzipFile (zipFile, targetFolder):
 def BuildSolution (msBuildPath, solutionPath, configuration):
 	print ('Building ' + solutionPath)
 	subprocess.call ([msBuildPath, solutionPath, '/property:Configuration=' + configuration, '/property:Platform=x64'])
-	pass
 
-def Main (argv):
-	if len (argv) != 3:
-		print 'usage: installdepswin.py <targetFolder> <msBuildPath>'
-		return 1
-	currentDir = os.path.dirname (os.path.abspath (__file__))
-	os.chdir (currentDir)
-
-	targetFolder = sys.argv[1]
-	msBuildPath = sys.argv[2] # "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
-
-	if not os.path.exists (targetFolder):
-		os.makedirs (targetFolder)
-	
+def InstallwxWidgets (targetFolder, msBuildPath):
 	wxWidgetsName = 'wxWidgets-3.1.2'
 	wxWidgetsZipUrl = 'https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.2/wxWidgets-3.1.2.7z'
 	wxWidgetsZipPath = os.path.join (targetFolder, wxWidgetsName + '.7z')
@@ -45,7 +32,23 @@ def Main (argv):
 		UnzipFile (wxWidgetsZipPath, wxWidgetsFolderPath)
 		solutionPath = os.path.join (wxWidgetsFolderPath, 'build', 'msw', 'wx_vc15.sln')
 		BuildSolution (msBuildPath, solutionPath, 'Debug')
-		BuildSolution (msBuildPath, solutionPath, 'Release')
+		BuildSolution (msBuildPath, solutionPath, 'Release')	
+
+def Main (argv):
+	if len (argv) != 3:
+		print 'usage: installdepswin.py <targetFolder> <msBuildPath>'
+		return 1
+	
+	currentDir = os.path.dirname (os.path.abspath (__file__))
+	os.chdir (currentDir)
+
+	targetFolder = sys.argv[1]
+	msBuildPath = sys.argv[2] # "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+
+	if not os.path.exists (targetFolder):
+		os.makedirs (targetFolder)
+	
+	InstallwxWidgets (targetFolder, msBuildPath)
 	return 0
 	
 sys.exit (Main (sys.argv))
