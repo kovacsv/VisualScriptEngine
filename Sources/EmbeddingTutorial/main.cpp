a43ba84ad417cbf597ecc533783c0194d5b930cc
kovacsv <viktorkovacs@gmail.com>
2020-05-20 22:57:03 +0200

    Add example node implementation to the tutorial's code.

--- Sources/EmbeddingTutorial/main.cpp
+++ Sources/EmbeddingTutorial/main.cpp
@@ -1,4 +1,6 @@
+#include "NE_SingleValues.hpp"
 #include "NUIE_NodeEditor.hpp"
+#include "BI_BasicUINode.hpp"
 
 class MyEventHandlers : public NUIE::EventHandlers
 {
@@ -141,10 +143,78 @@ private:
 	NE::EvaluationEnv			evaluationEnv;
 };
 
+class MyNode : public BI::BasicUINode
+{
+	DYNAMIC_SERIALIZABLE (MyNode);
+
+public:
+	MyNode ()
+	{
+	}
+
+	MyNode (const NE::String& name, const NUIE::Point& position) :
+		BI::BasicUINode (name, position)
+	{
+	}
+
+	virtual ~MyNode ()
+	{
+	}
+
+	virtual void Initialize () override
+	{
+		RegisterUIInputSlot (NUIE::UIInputSlotPtr (new NUIE::UIInputSlot (
+			NE::SlotId ("a"), NE::String (L"A"), NE::ValuePtr (new NE::DoubleValue (0.0)),
+			NE::OutputSlotConnectionMode::Single
+		)));
+		RegisterUIInputSlot (NUIE::UIInputSlotPtr (new NUIE::UIInputSlot (
+			NE::SlotId ("b"), NE::String (L"B"), NE::ValuePtr (new NE::DoubleValue (0.0)),
+			NE::OutputSlotConnectionMode::Single
+		)));
+		RegisterUIOutputSlot (NUIE::UIOutputSlotPtr (new NUIE::UIOutputSlot (
+			NE::SlotId ("result"), NE::String (L"Result")
+		)));
+	}
+
+	virtual NE::ValueConstPtr Calculate (NE::EvaluationEnv& env) const override
+	{
+		NE::ValueConstPtr aValue = EvaluateInputSlot (NE::SlotId ("a"), env);
+		NE::ValueConstPtr bValue = EvaluateInputSlot (NE::SlotId ("b"), env);
+		if (!NE::IsSingleType<NE::NumberValue> (aValue) ||
+			!NE::IsSingleType<NE::NumberValue> (bValue))
+		{
+			return nullptr;
+		}
+
+		double aDouble = NE::NumberValue::ToDouble (aValue);
+		double bDouble = NE::NumberValue::ToDouble (bValue);
+		return NE::ValueConstPtr (new NE::DoubleValue (aDouble + bDouble));
+	}
+
+	virtual NE::Stream::Status Read (NE::InputStream& inputStream) override
+	{
+		NE::ObjectHeader header (inputStream);
+		BasicUINode::Read (inputStream);
+		return inputStream.GetStatus ();
+	}
+
+	virtual NE::Stream::Status Write (NE::OutputStream& outputStream) const override
+	{
+		NE::ObjectHeader header (outputStream, serializationInfo);
+		BasicUINode::Write (outputStream);
+		return outputStream.GetStatus ();
+	}
+};
+
+DYNAMIC_SERIALIZATION_INFO (MyNode, 1, "{8DC2C2F6-C1D1-451A-96C5-D64B3DEDB764}");
+
 int main (int argc, char* argv[])
 {
 	MyNodeUIEnvironment uiEnvironment;
 	NUIE::NodeEditor nodeEditor (uiEnvironment);
+
+	nodeEditor.AddNode (NUIE::UINodePtr (new MyNode (NE::String (L"My Node"), NUIE::Point (0.0, 0.0))));
+
 	nodeEditor.Update ();
 	nodeEditor.Draw ();
 
