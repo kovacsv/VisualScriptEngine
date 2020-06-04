d6a1561bae3da975acae215009d5679ed129edb3
kovacsv <viktorkovacs@gmail.com>
2020-06-05 00:22:19 +0200

    Update first compatible version.

--- Sources/NodeUIEngine/NUIE_VersionCompatibility.cpp
+++ Sources/NodeUIEngine/NUIE_VersionCompatibility.cpp
@@ -6,7 +6,7 @@ namespace NUIE
 
 static const Version& GetFirstCompatibleVersion ()
 {
-	static const Version FirstCompatibleVersion (0, 3, 1);
+	static const Version FirstCompatibleVersion (0, 3, 2);
 	return FirstCompatibleVersion;
 }
 
