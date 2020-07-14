759771f1899ce0b10c79e902270bb1596146e905
kovacsv <viktorkovacs@gmail.com>
2020-07-14 15:56:25 +0200

    Separate devkit and package builder scripts.

---
+++ CreateDevKit.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import re
+import subprocess
+import zipfile
+
+def PrintInfo (message):
+	print 'INFO: ' + message
+
+def PrintError (message):
+	print 'ERROR: ' + message
+	
+def Main (argv):
+	currentDir = os.path.dirname (os.path.abspath (__file__))
+	os.chdir (currentDir)
+	
+	if len (argv) != 3:
+		print 'usage: CreateDevKit.py <msBuildPath> <msBuildConfiguration>'
+		return 1
+
+	msBuildPath = argv[1]
+	msBuildConfiguration = argv[2]
+
+	installProjectPath = os.path.join ('Build', 'INSTALL.vcxproj')
+	buildResult = subprocess.call ([
+		msBuildPath,
+		installProjectPath,
+		'-verbosity:m',
+		'/property:Configuration=' + msBuildConfiguration
+	])
+	buildResult = 0
+	if buildResult != 0:
+		PrintError ('Failed to build install project.')
+		return 1
+	
+	return 0
+	
+sys.exit (Main (sys.argv))
--- CreatePackage.py
+++ CreatePackage.py
@@ -23,34 +23,20 @@ def Main (argv):
 	currentDir = os.path.dirname (os.path.abspath (__file__))
 	os.chdir (currentDir)
 	
-	if len (argv) != 4:
-		print 'usage: createpackagewin.py <visualStudioVersion> <msBuildPath> <msBuildConfiguration>'
+	if len (argv) != 2:
+		print 'usage: CreatePackage.py <buildIdentifier>'
 		return 1
 
-	visualStudioVersion = argv[1]
-	msBuildPath = argv[2]
-	msBuildConfiguration = argv[3]
+	buildIdentifier = argv[1]
 
 	cMakeListsPath = os.path.abspath ('CMakeLists.txt')
 	version = GetVersionFromCMakeLists (cMakeListsPath)
 
-	installProjectPath = os.path.join ('Build', 'INSTALL.vcxproj')
-	buildResult = subprocess.call ([
-		msBuildPath,
-		installProjectPath,
-		'-verbosity:m',
-		'/property:Configuration=' + msBuildConfiguration
-	])
-	buildResult = 0
-	if buildResult != 0:
-		PrintError ('Failed to build install project.')
-		return 1
-	
 	packagePath = os.path.abspath (os.path.join ('Build', 'Package'))
 	if not os.path.exists (packagePath):
 		os.makedirs (packagePath)
 
-	zipPath = os.path.abspath (os.path.join (packagePath, 'VisualScriptEngine_V' + version + '_VS' + visualStudioVersion + '_' + msBuildConfiguration + '_x64.zip'))
+	zipPath = os.path.abspath (os.path.join (packagePath, 'VisualScriptEngine_V' + version + '_' + buildIdentifier + '_x64.zip'))
 	requiredFolders = [
 		os.path.abspath (os.path.join ('Build', 'DevKit', 'include')),
 		os.path.abspath (os.path.join ('Build', 'DevKit', 'lib')),
