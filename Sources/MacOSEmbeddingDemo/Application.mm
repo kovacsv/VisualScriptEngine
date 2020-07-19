8063a43ba0e97421004d4069dd4717545a0ec0b7
Viktor Kovacs <viktorkovacs@gmail.com>
2020-07-19 10:27:54 +0200

    Custom skin parameters for test application.

--- Sources/MacOSEmbeddingDemo/Application.mm
+++ Sources/MacOSEmbeddingDemo/Application.mm
@@ -47,6 +47,46 @@
 
 @end
 
+static const NUIE::BasicSkinParams& GetAppSkinParams ()
+{
+	static const NUIE::BasicSkinParams skinParams (
+		/*backgroundColor*/ NUIE::Color (250, 250, 250),
+		/*connectionLinePen*/ NUIE::Pen (NUIE::Color (38, 50, 56), 1.0),
+		/*nodePadding*/ 5.0,
+		/*nodeBorderPen*/ NUIE::Pen (NUIE::Color (38, 50, 56), 1.0),
+		/*nodeHeaderTextFont*/ NUIE::Font (L"Arial", 16.0),
+		/*nodeHeaderTextColor*/ NUIE::Color (250, 250, 250),
+		/*nodeHeaderErrorTextColor*/ NUIE::Color (250, 250, 250),
+		/*nodeHeaderBackgroundColor*/ NUIE::Color (41, 127, 255),
+		/*nodeHeaderErrorBackgroundColor*/ NUIE::Color (199, 80, 80),
+		/*nodeContentTextFont*/ NUIE::Font (L"Arial", 14.0),
+		/*nodeContentTextColor*/ NUIE::Color (0, 0, 0),
+		/*nodeContentBackgroundColor*/ NUIE::Color (236, 236, 236),
+		/*slotTextColor*/ NUIE::Color (0, 0, 0),
+		/*slotTextBackgroundColor*/ NUIE::Color (246, 246, 246),
+		/*needToDrawSlotCircles*/ true,
+		/*slotCircleSize*/ NUIE::Size (8.0, 8.0),
+		/*selectionBlendColor*/ NUIE::BlendColor (NUIE::Color (41, 127, 255), 0.25),
+		/*disabledBlendColor*/ NUIE::BlendColor (NUIE::Color (0, 138, 184), 0.2),
+		/*selectionRectPen*/ NUIE::Pen (NUIE::Color (41, 127, 255), 1.0),
+		/*nodeSelectionRectPen*/ NUIE::Pen (NUIE::Color (41, 127, 255), 3.0),
+		/*buttonBorderPen*/ NUIE::Pen (NUIE::Color (146, 152, 155), 1.0),
+		/*buttonBackgroundColor*/ NUIE::Color (217, 217, 217),
+		/*textPanelTextColor*/ NUIE::Color (0, 0, 0),
+		/*textPanelBackgroundColor*/ NUIE::Color (236, 236, 236),
+		/*groupNameFont*/ NUIE::Font (L"Arial", 16.0),
+		/*groupNameColor*/ NUIE::Color (0, 0, 0),
+		/*groupBackgroundColors*/ NUIE::NamedColorSet ({
+			{ NE::LocalizeString (L"Blue"), NUIE::Color (160, 200, 240) },
+			{ NE::LocalizeString (L"Green"), NUIE::Color (160, 239, 160) },
+			{ NE::LocalizeString (L"Red"), NUIE::Color (239, 189, 160) }
+		}),
+		/*groupPadding*/ 10.0
+	);
+	return skinParams;
+}
+
+
 static void InitNodeTree (NUIE::NodeTree& nodeTree)
 {
 	size_t inputNodes = nodeTree.AddGroup (L"Input Nodes");
@@ -161,7 +201,7 @@ bool AppEventHandler::OnParameterSettings(NUIE::ParameterInterfacePtr paramAcces
 
 AppNodeUIEnvironment::AppNodeUIEnvironment () :
 	stringConverter (NE::GetDefaultStringConverter ()),
-	skinParams (NUIE::GetDefaultSkinParams ()),
+	skinParams (GetAppSkinParams ()),
 	evaluationEnv (NE::EmptyEvaluationEnv),
 	eventHandler (),
 	clipboardHandler (),
