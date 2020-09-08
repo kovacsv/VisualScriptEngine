e8e583132fddcdb1eb56a74208c934b56fd6fcc4
Viktor Kovacs <viktorkovacs@gmail.com>
2020-09-08 19:05:44 +0200

    Add logo with text.

--- Tools/GenerateLogoPngs.py
+++ Tools/GenerateLogoPngs.py
@@ -3,12 +3,11 @@ import sys
 import re
 import subprocess
 
-def GeneratePng (inkscapePath, sourceSvgPath, resultPngPath, size):
+def GeneratePng (inkscapePath, sourceSvgPath, resultPngPath, height):
 	command = [
 		inkscapePath,
 		'--export-png=' + resultPngPath,
-		'--export-width=' + str (size),
-		'--export-height=' + str (size),
+		'--export-height=' + str (height),
 		sourceSvgPath
 	]
 	subprocess.call (command)
@@ -24,14 +23,14 @@ def Main (argv):
 	inkscapePath = sys.argv[1] # "C:\Program Files\Inkscape\inkscape.com"
 	
 	logoFolder = os.path.abspath (os.path.join ('..', 'Documentation', 'Logo'))
-	for logoName in ['vse_logo', 'vse_logo_light', 'vse_logo_dark']:
+	for logoName in ['vse_logo', 'vse_logo_light', 'vse_logo_dark', 'vse_logo_text_dark']:
 		logoSvgPath = os.path.join (logoFolder, 'svg', logoName + '.svg')
-		for size in [16, 24, 32, 48, 64, 128, 256, 512]:
-			sizeStr = str (size)
-			while len (sizeStr) < 3:
-				sizeStr = '0' + sizeStr
-			pngPath = os.path.join (logoFolder, 'png', logoName + '_' + sizeStr + '.png')
-			GeneratePng (inkscapePath, logoSvgPath, pngPath, size)
+		for height in [16, 24, 32, 48, 64, 128, 256, 512]:
+			heightStr = str (height)
+			while len (heightStr) < 3:
+				heightStr = '0' + heightStr
+			pngPath = os.path.join (logoFolder, 'png', logoName + '_' + heightStr + '.png')
+			GeneratePng (inkscapePath, logoSvgPath, pngPath, height)
 
 	return 0
 	
