378ba66cef8891bdd4386720f7131ab25169b0db
Viktor Kovacs <viktorkovacs@gmail.com>
2020-09-16 06:52:37 +0200

    Copy pdb files to the package on windows.

--- CreatePackage.py
+++ CreatePackage.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import re
-import subprocess
+import platform
 import zipfile
 
 def PrintInfo (message):
@@ -23,21 +23,23 @@ def Main (argv):
 	currentDir = os.path.dirname (os.path.abspath (__file__))
 	os.chdir (currentDir)
 	
-	if len (argv) != 3:
-		print 'usage: CreatePackage.py <devKitDir> <buildIdentifier>'
+	if len (argv) != 4:
+		print 'usage: CreatePackage.py <devKitDir> <packageName> <buildType>'
 		return 1
 
 	devKitDir = argv[1]
-	buildIdentifier = argv[2]
+	packageName = argv[2]
+	buildType = argv[3]
 
 	cMakeListsPath = os.path.abspath ('CMakeLists.txt')
 	version = GetVersionFromCMakeLists (cMakeListsPath)
 
-	packagePath = os.path.abspath (os.path.join ('Build', 'Package'))
+	buildDir = os.path.abspath ('Build')
+	packagePath = os.path.join (buildDir, 'Package')
 	if not os.path.exists (packagePath):
 		os.makedirs (packagePath)
 
-	zipPath = os.path.abspath (os.path.join (packagePath, 'VisualScriptEngine_V' + version + '_' + buildIdentifier + '_x64.zip'))
+	zipPath = os.path.abspath (os.path.join (packagePath, 'VisualScriptEngine_V' + version + '_' + packageName + '_' + buildType + '_x64.zip'))
 	requiredFolders = [
 		os.path.abspath (os.path.join (devKitDir, 'include')),
 		os.path.abspath (os.path.join (devKitDir, 'lib')),
@@ -48,6 +50,13 @@ def Main (argv):
 	for folder in requiredFolders:
 		for file in os.listdir (folder):
 			zip.write (os.path.join (folder, file), os.path.join (os.path.basename (folder), file))	
+
+	if (platform.system () == 'Windows' and buildType in ['Debug', 'RelWithDebInfo']):
+		libPath = os.path.abspath (os.path.join (devKitDir, 'lib'))
+		for module in ['NodeEngine', 'NodeUIEngine', 'BuiltInNodes', 'WindowsAppSupport']:
+			pdbPath = os.path.join (buildDir, module + '.dir', buildType, module + '.pdb')
+			zip.write (pdbPath, os.path.join (os.path.basename (libPath), module + '.pdb'))
+
 	zip.close ()
 	
 	return 0
