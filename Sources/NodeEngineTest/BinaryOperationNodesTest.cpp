deaa65d05bb64e3486a50097ea8e03f30ca920be
Viktor Kovacs <viktorkovacs@gmail.com>
2020-10-23 22:10:52 +0200

    Separate tests for binary operation nodes.

--- Sources/NodeEngineTest/BinaryOperationNodesTest.cpp
+++ Sources/NodeEngineTest/BinaryOperationNodesTest.cpp
@@ -11,31 +11,68 @@ using namespace BI;
 namespace BinaryOperationNodesTest
 {
 
-TEST (TestAllBinaryOperationNodes)
+TEST (TestAdditionNode)
 {
 	TestUIEnvironment env;
 	NodeUIManager uiManager (env);
 
 	UINodePtr val1 = uiManager.AddNode (UINodePtr (new DoubleUpDownNode (LocString (L"Value1"), Point (0, 0), 1.0, 1.0)), EmptyEvaluationEnv);
 	UINodePtr val2 = uiManager.AddNode (UINodePtr (new DoubleUpDownNode (LocString (L"Value1"), Point (0, 0), 2.0, 1.0)), EmptyEvaluationEnv);
-	UINodePtr val3 = uiManager.AddNode (UINodePtr (new DoubleUpDownNode (LocString (L"Value3"), Point (0, 0), 3.0, 1.0)), EmptyEvaluationEnv);
-	UINodePtr add = uiManager.AddNode (UINodePtr (new AdditionNode (LocString (L"Addition"), Point (0, 0))), EmptyEvaluationEnv);
-	UINodePtr sub = uiManager.AddNode (UINodePtr (new SubtractionNode (LocString (L"Subtracion"), Point (0, 0))), EmptyEvaluationEnv);
-	UINodePtr mul = uiManager.AddNode (UINodePtr (new MultiplicationNode (LocString (L"Multiplication"), Point (0, 0))), EmptyEvaluationEnv);
-	UINodePtr div = uiManager.AddNode (UINodePtr (new DivisionNode (LocString (L"Division"), Point (0, 0))), EmptyEvaluationEnv);
+	UINodePtr op = uiManager.AddNode (UINodePtr (new AdditionNode (LocString (L"Addition"), Point (0, 0))), EmptyEvaluationEnv);
+	uiManager.ConnectOutputSlotToInputSlot (val1->GetUIOutputSlot (SlotId ("out")), op->GetUIInputSlot (SlotId ("a")));
+	uiManager.ConnectOutputSlotToInputSlot (val2->GetUIOutputSlot (SlotId ("out")), op->GetUIInputSlot (SlotId ("b")));
 
-	uiManager.ConnectOutputSlotToInputSlot (val1->GetUIOutputSlot (SlotId ("out")), add->GetUIInputSlot (SlotId ("a")));
-	uiManager.ConnectOutputSlotToInputSlot (val2->GetUIOutputSlot (SlotId ("out")), add->GetUIInputSlot (SlotId ("b")));
-	uiManager.ConnectOutputSlotToInputSlot (val1->GetUIOutputSlot (SlotId ("out")), sub->GetUIInputSlot (SlotId ("a")));
-	uiManager.ConnectOutputSlotToInputSlot (val2->GetUIOutputSlot (SlotId ("out")), sub->GetUIInputSlot (SlotId ("b")));
-	uiManager.ConnectOutputSlotToInputSlot (add->GetUIOutputSlot (SlotId ("result")), mul->GetUIInputSlot (SlotId ("a")));
-	uiManager.ConnectOutputSlotToInputSlot (sub->GetUIOutputSlot (SlotId ("result")), mul->GetUIInputSlot (SlotId ("b")));
-	uiManager.ConnectOutputSlotToInputSlot (mul->GetUIOutputSlot (SlotId ("result")), div->GetUIInputSlot (SlotId ("a")));
-	uiManager.ConnectOutputSlotToInputSlot (val3->GetUIOutputSlot (SlotId ("out")), div->GetUIInputSlot (SlotId ("b")));
+	ValueConstPtr val = op->Evaluate (EmptyEvaluationEnv);
+	ASSERT (IsSingleType<DoubleValue> (val));
+	ASSERT (IsEqual (DoubleValue::Get (CreateSingleValue (val)), 3.0));
+}
 
-	ValueConstPtr val = div->Evaluate (EmptyEvaluationEnv);
+TEST (TestSubtractionNode)
+{
+	TestUIEnvironment env;
+	NodeUIManager uiManager (env);
+
+	UINodePtr val1 = uiManager.AddNode (UINodePtr (new DoubleUpDownNode (LocString (L"Value1"), Point (0, 0), 1.0, 1.0)), EmptyEvaluationEnv);
+	UINodePtr val2 = uiManager.AddNode (UINodePtr (new DoubleUpDownNode (LocString (L"Value1"), Point (0, 0), 2.0, 1.0)), EmptyEvaluationEnv);
+	UINodePtr op = uiManager.AddNode (UINodePtr (new SubtractionNode (LocString (L"Subtraction"), Point (0, 0))), EmptyEvaluationEnv);
+	uiManager.ConnectOutputSlotToInputSlot (val1->GetUIOutputSlot (SlotId ("out")), op->GetUIInputSlot (SlotId ("a")));
+	uiManager.ConnectOutputSlotToInputSlot (val2->GetUIOutputSlot (SlotId ("out")), op->GetUIInputSlot (SlotId ("b")));
+
+	ValueConstPtr val = op->Evaluate (EmptyEvaluationEnv);
 	ASSERT (IsSingleType<DoubleValue> (val));
 	ASSERT (IsEqual (DoubleValue::Get (CreateSingleValue (val)), -1.0));
+}
+
+TEST (TestMultiplicationNode)
+{
+	TestUIEnvironment env;
+	NodeUIManager uiManager (env);
+
+	UINodePtr val1 = uiManager.AddNode (UINodePtr (new DoubleUpDownNode (LocString (L"Value1"), Point (0, 0), 1.0, 1.0)), EmptyEvaluationEnv);
+	UINodePtr val2 = uiManager.AddNode (UINodePtr (new DoubleUpDownNode (LocString (L"Value1"), Point (0, 0), 2.0, 1.0)), EmptyEvaluationEnv);
+	UINodePtr op = uiManager.AddNode (UINodePtr (new MultiplicationNode (LocString (L"Multiplication"), Point (0, 0))), EmptyEvaluationEnv);
+	uiManager.ConnectOutputSlotToInputSlot (val1->GetUIOutputSlot (SlotId ("out")), op->GetUIInputSlot (SlotId ("a")));
+	uiManager.ConnectOutputSlotToInputSlot (val2->GetUIOutputSlot (SlotId ("out")), op->GetUIInputSlot (SlotId ("b")));
+
+	ValueConstPtr val = op->Evaluate (EmptyEvaluationEnv);
+	ASSERT (IsSingleType<DoubleValue> (val));
+	ASSERT (IsEqual (DoubleValue::Get (CreateSingleValue (val)), 2.0));
+}
+
+TEST (TestDivisionNode)
+{
+	TestUIEnvironment env;
+	NodeUIManager uiManager (env);
+
+	UINodePtr val1 = uiManager.AddNode (UINodePtr (new DoubleUpDownNode (LocString (L"Value1"), Point (0, 0), 1.0, 1.0)), EmptyEvaluationEnv);
+	UINodePtr val2 = uiManager.AddNode (UINodePtr (new DoubleUpDownNode (LocString (L"Value1"), Point (0, 0), 2.0, 1.0)), EmptyEvaluationEnv);
+	UINodePtr op = uiManager.AddNode (UINodePtr (new DivisionNode (LocString (L"Division"), Point (0, 0))), EmptyEvaluationEnv);
+	uiManager.ConnectOutputSlotToInputSlot (val1->GetUIOutputSlot (SlotId ("out")), op->GetUIInputSlot (SlotId ("a")));
+	uiManager.ConnectOutputSlotToInputSlot (val2->GetUIOutputSlot (SlotId ("out")), op->GetUIInputSlot (SlotId ("b")));
+
+	ValueConstPtr val = op->Evaluate (EmptyEvaluationEnv);
+	ASSERT (IsSingleType<DoubleValue> (val));
+	ASSERT (IsEqual (DoubleValue::Get (CreateSingleValue (val)), 0.5));
 }
 
 }
