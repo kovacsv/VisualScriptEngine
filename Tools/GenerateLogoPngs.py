86d77231d180626b4e7bb0e41cb98b5c23873814
Viktor Kovacs <viktorkovacs@gmail.com>
2020-12-16 17:59:14 +0100

    Modify the names of the logo files.

--- Tools/GenerateLogoPngs.py
+++ Tools/GenerateLogoPngs.py
@@ -22,18 +22,23 @@ def Main (argv):
 	inkscapePath = sys.argv[1] # "C:\Program Files\Inkscape\inkscape.com"
 	
 	logoFolder = os.path.abspath (os.path.join ('..', 'Documentation', 'Logo'))
-	for logoName in ['vse_logo', 'vse_logo_light', 'vse_logo_text', 'vse_icon']:
-		logoSvgPath = os.path.join (logoFolder, 'svg', logoName + '.svg')
+	logoSvgFolder = os.path.join (logoFolder, 'svg')
+	logoPngFolder = os.path.join (logoFolder, 'png')
+	if not os.path.exists (logoPngFolder):
+		os.makedirs (logoPngFolder)
+	
+	for logoName in ['vse_logo', 'vse_logo_simple_dark', 'vse_logo_simple_light', 'vse_logo_simple_text']:
+		logoSvgPath = os.path.join (logoSvgFolder, logoName + '.svg')
 		for height in [16, 24, 32, 48, 64, 128, 256, 512]:
 			heightStr = str (height)
 			while len (heightStr) < 3:
 				heightStr = '0' + heightStr
-			pngPath = os.path.join (logoFolder, 'png', logoName + '_' + heightStr + '.png')
-			GeneratePng (inkscapePath, logoSvgPath, pngPath, height)
+			logoPngPath = os.path.join (logoPngFolder, logoName + '_' + heightStr + '.png')
+			GeneratePng (inkscapePath, logoSvgPath, logoPngPath, height)
 	GeneratePng (
 		inkscapePath,
-		os.path.join (logoFolder, 'svg', 'vse_logo_text_sm.svg'),
-		os.path.join (logoFolder, 'png', 'vse_logo_text_sm.png'),
+		os.path.join (logoSvgFolder, 'vse_logo_sm.svg'),
+		os.path.join (logoPngFolder, 'vse_logo_sm.png'),
 		640)
 
 	return 0
