04d62e6454ffa0285ead5527c901c0cc75a4c3f0
Viktor Kovacs <viktorkovacs@gmail.com>
2020-12-31 20:47:23 +0100

    GCC build fix.

--- Sources/NodeUIEngine/NUIE_HashUtils.hpp
+++ Sources/NodeUIEngine/NUIE_HashUtils.hpp
@@ -1,12 +1,14 @@
 #ifndef NUIE_HASHUTILS_HPP
 #define NUIE_HASHUTILS_HPP
 
+#include <cstddef>
+
 struct EnumHash
 {
 	template <typename EnumType>
-	size_t operator() (EnumType enumVal) const
+	std::size_t operator() (EnumType enumVal) const
 	{
-		return static_cast<size_t> (enumVal);
+		return static_cast<std::size_t> (enumVal);
 	}
 };
 
