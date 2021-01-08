6b164425822bf2dfe4ac57f65d99f43c00950a62
Viktor Kovacs <viktorkovacs@gmail.com>
2021-01-08 12:03:33 +0100

    Generate full html for PVS Studio.

--- Tools/PVSStudio.bat
+++ Tools/PVSStudio.bat
@@ -1,3 +1,3 @@
 "C:\Program Files (x86)\PVS-Studio\PVS-Studio_Cmd.exe" --target ../Build/VisualScriptEngine.sln --excludeProjects NodeEngineTest --rulesConfig rules.pvsconfig --output ../Build/Debug/VisualScriptEngine.plog
-"C:\Program Files (x86)\PVS-Studio\PlogConverter.exe" ../Build/Debug/VisualScriptEngine.plog --outputDir ../Build/Debug -t Html
-start ..\Build\Debug\VisualScriptEngine.plog.html
+"C:\Program Files (x86)\PVS-Studio\PlogConverter.exe" ../Build/Debug/VisualScriptEngine.plog --outputDir ../Build/Debug -t FullHtml
+start ..\Build\Debug\fullhtml\index.html
